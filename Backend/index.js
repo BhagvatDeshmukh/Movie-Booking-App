ba64b5b5e04890f81646b21d0f81b8e151ef6936
BhagvatDeshmukh <134522509+BhagvatDeshmukh@users.noreply.github.com>
2024-08-07 15:15:13 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.BASE_PORT || 3000;
 const saltRounds = 10;
 env.config();
 
-const corsWhitelist = ['http://localhost:8000','http://10.210.208.213:8000'];
+const corsWhitelist = ['https://movie-booking-app-frontend.onrender.com:8000','https://movie-booking-app-frontend.onrender.com:8000'];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
